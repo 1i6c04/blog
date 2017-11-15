2587a759517018cae55aed9324814e80feb81723
l1i6c04 <com45167gmail.com>
2017-11-15 14:59:09 +0800

    Chapter 6 finished

--- blog/main/views.py
+++ blog/main/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.core.paginator import Page
+import datetime
 
 
 def main(request):
     '''
-    Show 'Hello world!' in the main page
+    Render the main page
     '''
-    return HttpResponse('Hello world!')
+    now = datetime.datetime.now()
+    context = {'like':'Django 很棒','now':now}
+    return render(request, 'main/main.html', context)
+def about(request):
+    '''
+    rander the about Page
+    '''
+    return render(request,'main/about.html')
+    
 # Create your views here.
